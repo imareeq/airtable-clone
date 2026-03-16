e6c9167e811e3e2c7c95db082c4f736437f6fc15
Areeq Imran <imranareeq@gmail.com>
2026-03-17 01:46:59 +1100

    feat: added favicon, colouring fixes

--- src/components/login-footer.tsx
+++ src/components/login-footer.tsx
@@ -3,14 +3,14 @@ export default function LoginFooter() {
     <div className="text-muted-foreground flex w-full flex-col gap-5 pt-15 text-xs font-light">
       <p>
         New to Airtable?{" "}
-        <a href="#" className="underline">
+        <a href="#" className="underline text-primary">
           Create an account
         </a>{" "}
         instead
       </p>
       <p>
         Manage your cookie preferences{" "}
-        <a href="#" className="underline">
+        <a href="#" className="underline text-primary">
           here
         </a>
       </p>
