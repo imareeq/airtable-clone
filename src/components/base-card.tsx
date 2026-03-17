12eb0599d828bda7ef591cefe03be136fb62c4fc
Areeq Imran <imranareeq@gmail.com>
2026-03-18 00:58:08 +1100

    feat: implemented the list view for bases. beginning implementation of DB schema and trpc backend to craete bases

--- src/components/base-card.tsx
+++ src/components/base-card.tsx
@@ -9,17 +9,11 @@ import {
   TrashIcon,
   UsersThreeIcon,
 } from "@phosphor-icons/react";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import { cn } from "~/lib/utils";
 import { useState } from "react";
+import BaseActionsDropdown from "./base-actions-dropdown";
 
 interface BaseCardProps {
   name: string;
@@ -74,42 +68,9 @@ export default function BaseCard({
             <StarIcon size={16} />
           </Button>
 
-          <DropdownMenu onOpenChange={setIsOpen}>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="h-8 w-8 rounded-lg bg-white"
-              >
-                <DotsThreeIcon size={20} weight="bold" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start" className="w-60 p-3">
-              <DropdownMenuItem className="gap-2 p-2 text-xs">
-                <PencilSimpleIcon className="size-4" /> Rename
-              </DropdownMenuItem>
-              <DropdownMenuItem className="gap-2 p-2 text-xs">
-                <CopyIcon className="size-4" /> Duplicate
-              </DropdownMenuItem>
-              <DropdownMenuItem className="gap-2 p-2 text-xs">
-                <ArrowRightIcon className="size-4" /> Move
-              </DropdownMenuItem>
-              <DropdownMenuItem className="gap-2 p-2 text-xs">
-                <UsersThreeIcon className="size-4" /> Go to workspace
-              </DropdownMenuItem>
-              <DropdownMenuItem className="gap-2 p-2 text-xs">
-                <PaintBrushHouseholdIcon className="size-4" /> Customize
-                appearance
-              </DropdownMenuItem>
-              <DropdownMenuSeparator className="my-3 px-2" />
-              <DropdownMenuItem className="gap-2 text-xs">
-                <TrashIcon className="size-4" /> Delete
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <BaseActionsDropdown baseId="123" onOpenChange={setIsOpen} />
         </div>
 
-        {/* Icon */}
         <div
           className="flex h-14 w-14 shrink-0 items-center justify-center rounded-2xl bg-[#1d70e6] text-2xl font-medium text-white"
           aria-hidden="true"
@@ -117,19 +78,16 @@ export default function BaseCard({
           {initials}
         </div>
 
-        {/* Text Area */}
         <div className="flex flex-col overflow-hidden">
           <h3 className="truncate text-xs leading-tight font-semibold text-[#1F2937]">
             {name}
           </h3>
 
           <div className="mt-1 flex items-center gap-1.5 text-xs">
-            {/* Default View */}
             <span className="text-muted-foreground group-hover:hidden">
               Opened {getRelativeTimeString(lastUpdated)}
             </span>
 
-            {/* Hover View */}
             <span className="hidden items-center gap-1.5 text-slate-500 group-hover:flex">
               <DatabaseIcon size={16} />
               Open data
--- src/components/base-list.tsx
+++ src/components/base-list.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import BaseListFilters from "./base-list-filters";
 import BaseCard from "./base-card";
+import BaseListItem from "./base-list-item";
+import { Separator } from "./ui/separator";
 
 export type ViewType = "list" | "grid";
 export type FilterType = "today" | "7days" | "30days" | "anytime";
@@ -20,13 +22,34 @@ export default function BaseList() {
         onFilterChange={setFilter}
       />
 
-      <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
-        <BaseCard
-          name={"Untitled Base"}
-          id={"123"}
-          lastUpdated={new Date()}
-        />
-      </div>
+      {view === "grid" && (
+        <div className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-4">
+          <BaseCard
+            name={"Untitled Base"}
+            id={"123"}
+            lastUpdated={new Date()}
+          />
+        </div>
+      )}
+
+      {view === "list" && (
+        <div className="flex w-full flex-col gap-2">
+          <div className="flex w-full flex-col gap-1 pb-3">
+            <div className="text-muted-foreground grid grid-cols-[1.5fr_1fr_1fr] items-center px-4 py-1.5 text-xs">
+              <p>Name</p>
+              <p className="">Last opened</p>
+              <p>Workspace</p>
+            </div>
+
+            <Separator />
+          </div>
+          <BaseListItem
+            name={"Untitled Base"}
+            lastUpdated={new Date()}
+            workspaceName={"My workspace"}
+          />
+        </div>
+      )}
     </div>
   );
 }
