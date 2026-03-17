c9c568e3160acc82ef4aafa2b573046db68cb00c
Areeq Imran <imranareeq@gmail.com>
2026-03-17 18:56:38 +1100

    implemented nav bar, pushed a few fixes for collapsible sidebar elements

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -1,10 +1,6 @@
-"use client"
+"use client";
 
-import {
-  Avatar,
-  AvatarFallback,
-  AvatarImage,
-} from "~/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,99 +9,207 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "~/components/ui/dropdown-menu"
+} from "~/components/ui/dropdown-menu";
 import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   useSidebar,
-} from "~/components/ui/sidebar"
-import { CaretUpDownIcon, SparkleIcon, CheckCircleIcon, CreditCardIcon, BellIcon, SignOutIcon } from "@phosphor-icons/react"
+} from "~/components/ui/sidebar";
+import { authClient } from "~/server/better-auth/client";
+import { useRouter } from "next/navigation";
+import { Separator } from "./ui/separator";
+import {
+  BellIcon,
+  CaretRightIcon,
+  EnvelopeSimpleIcon,
+  LinkIcon,
+  PaletteIcon,
+  SignOutIcon,
+  StarIcon,
+  TranslateIcon,
+  TrashIcon,
+  UserIcon,
+  UsersIcon,
+  WrenchIcon,
+} from "@phosphor-icons/react";
+import type { ReactNode } from "react";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string
-    email: string
-    avatar: string
+export type MenuItem = {
+  title: string;
+  icon: ReactNode;
+  secondary?: ReactNode;
+};
+
+const menuItems: MenuItem[] = [
+  {
+    title: "Account",
+    icon: <UserIcon className="size-4" />,
+  },
+  {
+    title: "Manage groups",
+    icon: <UsersIcon className="size-4" />,
+    secondary: <Badge className="bg-[#c4ecff] text-[#0f68a2]">Business</Badge>,
+  },
+  {
+    title: "Notification preferences",
+    icon: <BellIcon className="size-4" />,
+    secondary: <CaretRightIcon />,
+  },
+  {
+    title: "Language preferences",
+    icon: <TranslateIcon className="size-4" />,
+    secondary: <CaretRightIcon />,
+  },
+  {
+    title: "Appearance",
+    icon: <PaletteIcon className="size-4" />,
+    secondary: <CaretRightIcon />,
+  },
+  {
+    title: "Contact sales",
+    icon: <EnvelopeSimpleIcon className="size-4" />,
+  },
+  {
+    title: "Upgrade",
+    icon: <StarIcon className="size-4" />,
+  },
+  {
+    title: "Tell a friend",
+    icon: <EnvelopeSimpleIcon className="size-4" />,
+  },
+  {
+    title: "Integrations",
+    icon: <LinkIcon className="size-4" />,
+  },
+  {
+    title: "Builder hub",
+    icon: <WrenchIcon className="size-4" />,
+  },
+  {
+    title: "Trash",
+    icon: <TrashIcon className="size-4" />,
+  },
+  {
+    title: "Sign out",
+    icon: <SignOutIcon className="size-4" />,
+  },
+];
+
+export function NavUser() {
+  const { isMobile } = useSidebar();
+  const { data: session } = authClient.useSession();
+  const router = useRouter();
+
+  if (!session) {
+    return null;
   }
-}) {
-  const { isMobile } = useSidebar()
+
+  const { user } = session;
+
+  const handleLogout = async () => {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/login");
+        },
+      },
+    });
+  };
 
   return (
-    <SidebarMenu>
+    <SidebarMenu className="w-auto pr-2">
       <SidebarMenuItem>
         <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <SidebarMenuButton
-              size="lg"
-              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              <Avatar className="h-8 w-8 rounded-lg">
-                <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+          <DropdownMenuTrigger asChild className="flex items-center justify-center w-fit rounded-full">
+            <button className="hover:bg-background! cursor-pointer rounded-full">
+              <Avatar className="h-6.5 w-6.5 rounded-full hover:shadow">
+                <AvatarImage src={user.image ?? ""} alt={user.name} />
+                <AvatarFallback className="rounded-full text-xs">
+                  {user.name[0]}
+                </AvatarFallback>
               </Avatar>
-              <div className="grid flex-1 text-left text-sm leading-tight group-data-[collapsible=icon]:hidden">
-                <span className="truncate font-medium">{user.name}</span>
-                <span className="truncate text-xs">{user.email}</span>
-              </div>
-              <CaretUpDownIcon className="ml-auto size-4 group-data-[collapsible=icon]:hidden" />
-            </SidebarMenuButton>
+            </button>
           </DropdownMenuTrigger>
           <DropdownMenuContent
-            className="w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg"
-            side={isMobile ? "bottom" : "right"}
+            className="text-muted-foregroun/75 mr-1 w-74 p-3"
+            side="bottom"
             align="end"
-            sideOffset={4}
           >
-            <DropdownMenuLabel className="p-0 font-normal">
-              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
-                </Avatar>
-                <div className="grid flex-1 text-left text-sm leading-tight">
-                  <span className="truncate font-medium">{user.name}</span>
-                  <span className="truncate text-xs">{user.email}</span>
-                </div>
-              </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <SparkleIcon
-                />
-                Upgrade to Pro
-              </DropdownMenuItem>
+              <DropdownMenuLabel className="pb-1 font-semibold">
+                {user.name}
+              </DropdownMenuLabel>
+              <DropdownMenuLabel className="text-muted-foreground pt-0 font-normal">
+                {user.email}
+              </DropdownMenuLabel>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <CheckCircleIcon
-                />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCardIcon
-                />
-                Billing
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <BellIcon
-                />
-                Notifications
-              </DropdownMenuItem>
+              {menuItems.slice(0, 5).map((item) => (
+                <DropdownMenuItem
+                  key={item.title}
+                  className="flex items-center justify-between p-2"
+                >
+                  <div className="flex items-center gap-2">
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </div>
+                  {item.secondary && <span>{item.secondary}</span>}
+                </DropdownMenuItem>
+              ))}
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>
-              <SignOutIcon
-              />
-              Log out
-            </DropdownMenuItem>
+            <DropdownMenuGroup>
+              {menuItems.slice(5, 8).map((item) => (
+                <DropdownMenuItem
+                  key={item.title}
+                  className="flex items-center justify-between p-2"
+                >
+                  <div className="flex items-center gap-2">
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </div>
+                  {item.secondary && <span>{item.secondary}</span>}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              {menuItems.slice(8, 10).map((item) => (
+                <DropdownMenuItem
+                  key={item.title}
+                  className="flex items-center justify-between p-2"
+                >
+                  <div className="flex items-center gap-2">
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </div>
+                  {item.secondary && <span>{item.secondary}</span>}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              {menuItems.slice(10).map((item) => (
+                <DropdownMenuItem
+                  key={item.title}
+                  className="flex items-center justify-between p-2"
+                  onClick={item.title === "Sign out" ? handleLogout : undefined}
+                >
+                  <div className="flex items-center gap-2">
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </div>
+                  {item.secondary && <span>{item.secondary}</span>}
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuGroup>
           </DropdownMenuContent>
         </DropdownMenu>
       </SidebarMenuItem>
     </SidebarMenu>
-  )
+  );
 }
--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 
-import { Label } from "~/components/ui/label"
-import { SidebarInput } from "~/components/ui/sidebar"
-import { MagnifyingGlassIcon } from "@phosphor-icons/react"
+import { Label } from "~/components/ui/label";
+import { SidebarInput } from "~/components/ui/sidebar";
+import { MagnifyingGlassIcon } from "@phosphor-icons/react";
 
 export function SearchForm({ ...props }: React.ComponentProps<"form">) {
   return (
@@ -13,11 +13,14 @@ export function SearchForm({ ...props }: React.ComponentProps<"form">) {
         </Label>
         <SidebarInput
           id="search"
-          placeholder="Type to search..."
-          className="h-8 pl-7"
+          placeholder="Search..."
+          className="bg-background h-8 rounded-full pl-10"
         />
-        <MagnifyingGlassIcon className="pointer-events-none absolute top-1/2 left-2 size-4 -translate-y-1/2 opacity-50 select-none" />
+        <MagnifyingGlassIcon className="pointer-events-none absolute top-1/2 left-4 size-4 -translate-y-1/2 opacity-75 select-none" />
+        <p className="text-muted-foreground/50 pointer-events-none absolute top-1/2 right-4 -translate-y-1/2 text-xs select-none">
+          ctrl K
+        </p>
       </div>
     </form>
-  )
+  );
 }
