4101755a29a5c865a3a8dff65fb05c604806087f
Areeq Imran <imranareeq@gmail.com>
2026-03-19 03:59:47 +1100

    fix: fixed type error

--- src/server/api/routers/base.ts
+++ src/server/api/routers/base.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, baseProcedure } from "~/server/api/trpc";
 import { BaseService } from "~/services/base-service";
+import { BaseColor } from "../../../../generated/prisma";
 
 export const BaseRouter = createTRPCRouter({
   getAll: protectedProcedure.query(async ({ ctx }) => {
@@ -12,13 +13,13 @@ export const BaseRouter = createTRPCRouter({
   }),
 
   create: protectedProcedure
-    .input(z.object({ name: z.string(), color: z.string().optional() }))
+    .input(z.object({ name: z.string(), color: z.nativeEnum(BaseColor).optional() }))
     .mutation(async ({ ctx, input }) => {
       return BaseService.create(ctx.db, ctx.session.user.id, input);
     }),
 
   update: baseProcedure
-    .input(z.object({ baseId: z.string(), name: z.string().optional(), color: z.string().optional() }))
+    .input(z.object({ baseId: z.string(), name: z.string().optional(), color: z.nativeEnum(BaseColor).optional() }))
     .mutation(async ({ ctx, input }) => {
       return BaseService.update(ctx.db, input.baseId, input);
     }),
