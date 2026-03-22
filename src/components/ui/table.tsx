c09da26350c1a323fbc94a3d5fb3fccd8e05f84e
Areeq Imran <imranareeq@gmail.com>
2026-03-23 03:09:34 +1100

    feat: table headers are now sticky. Switched create column button to be an absolutely positioned table header

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -8,7 +8,7 @@ function Table({ className, ...props }: React.ComponentProps<"table">) {
   return (
     <div
       data-slot="table-container"
-      className="relative w-full overflow-x-auto"
+      className="relative w-full"
     >
       <table
         data-slot="table"
@@ -70,7 +70,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
     <th
       data-slot="table-head"
       className={cn(
-        "text-foreground h-8 min-w-21 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0",
+        "text-foreground h-8 px-2 text-left align-middle font-medium whitespace-nowrap [&:has([role=checkbox])]:pr-0",
         className,
       )}
       {...props}
@@ -83,7 +83,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
     <td
       data-slot="table-cell"
       className={cn(
-        "h-8 max-w-45! min-w-21 border p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0",
+        "h-8 max-w-45! border p-2 align-middle whitespace-nowrap [&:has([role=checkbox])]:pr-0",
         className,
       )}
       {...props}
